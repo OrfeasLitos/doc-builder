c633889b851280964ccfd1792df4671bbd1765de
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2020-04-05 13:04:27 +0300

    Parse scripts directories and dependencies

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const cmd = require('command-line-args')
 const path = require('path')
 const fs = require('fs')
@@ -15,7 +17,7 @@ module.exports = {
 
     const options = cmd(
       [
-        { name: 'scripts', alias: 's', type: String },
+        { name: 'scripts', alias: 's', type: String, multiple: true },
         { name: 'dependencies', alias: 'd', type: String }
       ],
       { argv: argv0 }
@@ -28,7 +30,23 @@ module.exports = {
     }
   },
 
-  findFiles: function(directory) {
-    return fs.readdirSync(directory)
+  findFiles: function(directories) {
+    return directories.flatMap(dir => {
+      return fs.readdirSync(dir).map(script => {
+        return path.parse(dir + script)
+      })
+    })
+  },
+
+  parseDeps(file) {
+    return fs.readFileSync(file, 'utf8')
+      .trim()
+      .split('\n')
+      .map((pair) => {
+        if (pair.indexOf('->') === -1) {
+          throw new TypeError(`Pair '${pair}' does not contain '->'`)
+        }
+        return pair.split('->').map(x => path.parse(x.trim()))
+      })
   }
 }
--- test/parse.js
+++ test/parse.js
@@ -1,26 +1,42 @@
-const {
-  findFiles,
-  parseCmd
-} = require('../src/parse')
+'use strict'
+
+const Parser = require('../src/parse')
 
 const path = require('path')
+const _ = require('lodash')
 require('should-sinon')
 
 describe('Command line parser', function() {
   function parsed() {
     return {
       tex: 'latex/main.tex',
-      scripts: 'example/public-sources/',
-      dependencies: 'deps.txt'
+      scripts: ['example/public-sources/', 'example/private-sources/'],
+      dependencies: 'example/deps.txt'
     }
   }
 
   // Useless test
-  it('findFiles should output a list of files', function() {
-    const files = findFiles(parsed().scripts)
+  it('findFiles should return a list of files', function() {
+    const files = Parser.findFiles(parsed().scripts)
+
     Array.isArray(files).should.be.true()
-    for (file of files) {
-      should.exist(path.parse(file))
+    for (const file of files) {
+      should.exist(file)
+    }
+  })
+
+  it('parseDeps should return a list of pairs of scripts', function() {
+    const scripts = Parser.findFiles(parsed().scripts)
+    const pairs = Parser.parseDeps(parsed().dependencies)
+
+    Array.isArray(pairs).should.be.true()
+    for (const pair of pairs) {
+      Array.isArray(pair).should.be.true()
+      for (const el of pair) {
+       scripts.some(file => _.isEqual(file, el)).should.be.true(
+         `Script '${path.format(el)}' not in ${parsed().scripts}`
+       )
+      }
     }
   })
 
